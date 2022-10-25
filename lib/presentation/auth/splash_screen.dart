f91253d191da96b964223f2ef7919ccee4246aef
godsfavourkio <kiofavour@gmail.com>
2022-10-25 09:00:17 +0000

    Refactored splash screen

--- lib/presentation/auth/splash_screen.dart
+++ lib/presentation/auth/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:lottie/lottie.dart';
+import 'package:qra/constants.dart';
 import 'package:qra/data/datasource/auth_local_datasource.dart';
 import 'package:qra/presentation/auth/intro_screen.dart';
 
@@ -28,24 +29,25 @@ class SplashScreen extends HookConsumerWidget {
       //     : Get.offNamedUntil(MyHomePage.id, (route) => false);
     });
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/qra_logo.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Transform.scale(
-            scale: 1,
-            child: Lottie.asset(
-              "assets/lottie/intro_qr.json",
-              animate: true,
-            ),
+      backgroundColor: Constants.coolOrange,
+      body: Center(
+        child: Transform.scale(
+          scale: 1,
+          child: Lottie.asset(
+            "assets/lottie/intro_qr.json",
+            animate: true,
           ),
         ),
       ),
     );
   }
 }
+
+//Container(
+//         width: double.infinity,
+//         decoration: const BoxDecoration(
+//           image: DecorationImage(
+//             image: AssetImage("assets/images/qra_logo.jpg"),
+//             fit: BoxFit.contain,
+//           ),
+//         ),
